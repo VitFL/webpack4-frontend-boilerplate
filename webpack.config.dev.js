b98d6cee7e29e077f30d9db33c9a5d1b6b3e7eb2
VitFL <vitfl@protonmail.com>
2018-12-13 23:39:00 +0200

    :sparkles: add BrowserSync plugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge'); // webpack-merge is used to merge common settings with development specific ones.
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const common = require('./webpack.common.js'); // Importing common webpack config
 
 module.exports = merge(common, {
@@ -14,6 +15,27 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './dist',
     hot: true,
+    port: 8081,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        // browse to http://localhost:3001/ during development
+        host: 'localhost',
+        port: 3001,
+        // proxy the Webpack Dev Server endpoint
+        // (which should be serving on http://localhost:3101/)
+        // through BrowserSync
+        proxy: 'http://localhost:8081/',
+      },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: false,
+      }
+    ),
+  ],
 });
